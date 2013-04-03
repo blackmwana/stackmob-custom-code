b95ca80c25660867c2687667677fa2cce4576aef
blackmwana <blackmwana@gmail.com>
2013-04-04 01:05:45 +0300

    Update Product.java

--- src/main/java/com/stackmob/example/Product.java
+++ src/main/java/com/stackmob/example/Product.java
@@ -65,28 +65,29 @@ public class Product implements CustomCodeMethod {
      if (!request.getBody().isEmpty()) {
      	try{
      		JSONObject rb= new JSONObject(request.getBody());
-     	 reqb = rb.toString();
+     	 	reqb = rb.toString();
      	 
      	 //here we collect all the categories and statii both in the new object
-       if (!rb.isNull("categories")){
-           JSONArray new_cats_ja= rb.getJSONArray("categories");
- 			if(new_cats_ja.length()>0){
-			new_cats = new String[new_cats_ja.length()];
-			for(int i=0;i<new_cats_ja.length();i++){
-				new_cats[i] = new_cats_ja.getString(i);//
+       		if (!rb.isNull("categories")){
+           		JSONArray new_cats_ja= rb.getJSONArray("categories");
+ 				if(new_cats_ja.length()>0){
+					new_cats = new String[new_cats_ja.length()];
+					for(int i=0;i<new_cats_ja.length();i++){
+						new_cats[i] = new_cats_ja.getString(i);//
 				//all_cats_hs.add(new_cats_ja.getString(i));
-			}	
+					}	
 			//all_cats=all_cats_hs.toArray();														}
-           }
-        if (!rb.isNull("statii")){
-           JSONArray new_statii_ja= rb.getJSONArray("statii");
-           if(new_statii_ja.length()>0){
+           		}
+       		}
+      		if (!rb.isNull("statii")){
+           		JSONArray new_statii_ja= rb.getJSONArray("statii");
+           		if(new_statii_ja.length()>0){
            			new_statii = new String[new_statii_ja.length()];
-				for(int i=0;i<new_statii_ja.length();i++){
-					new_statii[i] = new_statii_ja.getString(i);
-				//all_statii_hs.add(new_statii_ja.getString(i));
-				}
-           }
+					for(int i=0;i<new_statii_ja.length();i++){
+						new_statii[i] = new_statii_ja.getString(i);
+						//all_statii_hs.add(new_statii_ja.getString(i));
+					}
+         		}
            //all_statii=all_statii_hs.toArray();
            }
     		if (verb.equalsIgnoreCase("post")){
