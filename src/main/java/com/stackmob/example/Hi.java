b4225c7d967aac9b7760c0b5e21e6d3f0ca7a075
blackmwana <blackmwana@gmail.com>
2013-04-04 17:44:11 -0400

    aha

--- src/main/java/com/stackmob/example/Hi.java
+++ src/main/java/com/stackmob/example/Hi.java
@@ -2,11 +2,12 @@ import java.lang.String;
 import java.util.Arrays;
 public class Hi {
    
-    int[] ints =new int[2];
+    int[] intsx =new int[2];
     public static void main(String[] args) {
-        String[] s;
+        String[] s=null;
         s= new String[1];
-        System.out.println("Hello, World");
+        s[0]="love";
+        System.out.println("Hello, World"+s[0]);
     }
 
 }
